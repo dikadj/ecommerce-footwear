ea63b9daace5853cc32a4a4b9360782aa2610378
Primadika Dwijantara <primadika.dwijantara@gmail.com>
2018-02-15 22:19:56 +0700

    adding cart template

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,49 @@ import SearchIcon from "react-icons/lib/go/search"
 import CartIcon from "react-icons/lib/ti/shopping-cart"
 import MenuIcon from "material-ui-icons/Menu"
 
+import $ from "jquery"
+
+const cartItems = (count) => {
+	let item = ""
+	for (let i = 0; i < count; i++) {
+		item += `
+			<div class="my-2 card container">
+				<div class="row my-2">
+					<img
+						class="col-5 h-100"
+						src="http://via.placeholder.com/100x100"
+						alt="Card image"
+					>
+					<div class="col-5">
+						<div
+							class="position-absolute w-100 d-flex justify-content-end"
+						>
+							<a
+								href="#"
+								style="
+									text-decoration: none;
+									color: #000;
+								"
+							>
+								x
+							</a>
+						</div>
+						<h5>
+							<small>
+								Item ${i + 1}
+							</small>
+						</h5>
+						<button>
+							Checkout
+						</button>
+					</div>
+				</div>
+			</div>
+		`
+	}
+	return item
+}
+
 const Header = () => (
 	<div className="header">
 		<Router>
@@ -28,28 +71,56 @@ const Header = () => (
 							Featured
 						</Link>
 					</div>
-					<div className="px-3">
+					<div
+						className="px-3"
+					>
 						<SearchIcon className="search-icon small ml-2" />
-						<Badge
-							badgeContent={
-								<span
-									className="rounded-circle w-50 h-50 d-flex justify-content-center align-items-center"
-									style={{
-										color: "#fff",
-										background: "rgb(90,90,250)",
-									}}
-								>
-									<small>3</small>
-								</span>
-							}
+						<a
+							href="#"
+							style={{
+								textDecoration: "none",
+								color: "rgb(30,30,30)",
+							}}
 						>
-							<CartIcon className="cart-icon ml-2" />
-						</Badge>
+							<Badge
+								badgeContent={
+									<span
+										className="rounded-circle w-50 h-50 d-flex justify-content-center align-items-center"
+										style={{
+											color: "#fff",
+											background: "rgb(90,90,250)",
+										}}
+									>
+										<small>3</small>
+									</span>
+								}
+								data-toggle="popover"
+								data-trigger="click"
+								data-html="true"
+								data-placement="bottom"
+								data-title={`
+									<div>
+										There are
+										<strong>${3} items</strong>
+										in your cart
+									</div>
+								`}
+								data-content={cartItems(3)}
+							>
+								<CartIcon
+									className="cart-icon ml-2"
+								/>
+							</Badge>
+						</a>
 					</div>
 				</div>
 			</div>
 		</Router>
 	</div>
 )
+
+$(() => {
+	$("[data-toggle='popover']").popover()
+})
 
 export default Header
