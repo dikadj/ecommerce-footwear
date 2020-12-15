d46ccb1ed6d5fe360876f66c5651adfc6772a8bd
Primadika Dwijantara <primadika.dwijantara@gmail.com>
2020-12-15 23:01:07 +0700

    Updates

---
+++ precache-manifest.ac8eecbfda7ae864f48d91c67b0a9b7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e700750b79f17f6b4aa7",
+    "url": "/ecommerce-footwear/static/css/main.9a499ecd.chunk.css"
+  },
+  {
+    "revision": "e700750b79f17f6b4aa7",
+    "url": "/ecommerce-footwear/static/js/main.0f9058aa.chunk.js"
+  },
+  {
+    "revision": "78d6c948a444f4602725",
+    "url": "/ecommerce-footwear/static/js/runtime~main.1c9770ea.js"
+  },
+  {
+    "revision": "d3751fc1e47050f1da2b",
+    "url": "/ecommerce-footwear/static/css/2.ebede0f7.chunk.css"
+  },
+  {
+    "revision": "d3751fc1e47050f1da2b",
+    "url": "/ecommerce-footwear/static/js/2.2d8ab636.chunk.js"
+  },
+  {
+    "revision": "0f597210aad8750bc71adf3e24862321",
+    "url": "/ecommerce-footwear/index.html"
+  }
+];
